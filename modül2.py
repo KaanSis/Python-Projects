22edcf72f343303f336d205a7779410246bc7f6a
KaanSmoke <68927305+KaanSmoke@users.noreply.github.com>
2021-05-10 12:22:18 +0300

    Add files via upload

---
+++ modül2.py
@@ -0,0 +1,24 @@
+import random
+
+while True:
+
+    lower = "abcdefghijklmnopqrstuvwxyz"
+
+    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    numbers = "0123456789"
+
+    symbols = "[]{}()*/#&:;.,_-"
+
+    all = lower + upper + numbers + symbols
+
+    userlength = int(input("Şifre uzunluğu:"))
+
+    password = "".join(random.sample(all, userlength))
+
+    if userlength > 16:
+        print("Enter less digits.")
+    elif userlength < 6:
+        print("Enter more digits")
+    else:
+        print(password)
