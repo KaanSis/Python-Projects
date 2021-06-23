bdf2896d5c7e47e6a1257690a60bcab9fce254cf
KaanSmoke <68927305+KaanSmoke@users.noreply.github.com>
2021-06-23 12:07:10 +0300

    Update permutation.py

--- permutation.py
+++ permutation.py
@@ -4,13 +4,16 @@ def perm(x, y):
     factorial2 = 1
 
     for i in range(1, x + 1):
-        factoriyel1 *= i
+        factorial1 *= i
 
     for i in range(1, x - y + 1):
         factorial2 *= i
 
     result = (factorial1) / (factorial2)
-    print(f"P({x},{y}) = {sonuc}")
+    print(f"P({x},{y}) = {result}")
 
 #(5 and 3 here is an example, you can enter any number here you want.)
 perm(5, 3)
+
+#The result
+P(5,3) = 60.0
