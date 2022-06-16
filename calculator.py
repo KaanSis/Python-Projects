8b334fc0b95cdd2d12034e74517ef5edc5aee06a
KaanSmoke <68927305+KaanSmoke@users.noreply.github.com>
2022-06-16 23:57:30 +0300

    Add files via upload
    A function based simple calculator.

---
+++ calculator.py
@@ -0,0 +1,40 @@
+#(defining the functions here)
+#----------------------------
+def add(num1,num2):
+    print(num1+num2)
+
+def sub(num1,num2):
+    if num1>num2:
+        print(num1-num2)
+    else:
+        print(num2-num1)
+
+def multp(num1,num2):
+    print(num1*num2)
+
+def div(num1,num2):
+    if num1>num2:
+        print(num1//num2)
+    else:
+        print(num2//num1)
+#----------------------------
+
+#(Taking the input here)
+num1 = int(input("enter number1:"))
+num2 = int(input("enter number2:"))
+
+print("1.ADD","2.SUB","3.MULTP","4.DIV")
+
+#choosing the operation here
+choice = int(input(("Choose an operation 1/2/3/4:")))
+
+if choice in (1,2,3,4):
+    
+    if choice == 1:
+        add(num1,num2)
+    elif choice == 2:
+        sub(num1,num2)
+    elif choice == 3:
+        multp(num1,num2)
+    elif choice == 4:
+        div(num1,num2)
